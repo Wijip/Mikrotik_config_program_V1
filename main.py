a593beab00cfeeb877f6ce2dd6518616f7d268fc
Wiji <wiji.prabowo0857@gmail.com>
2023-10-10 14:01:27 +0700

    repair main.py

--- main.py
+++ main.py
@@ -1,105 +1,169 @@
-import logging, time
-from mikrotik_connection import MikrotikConnection
-from manual_config import Manual_config
+import os, logging
 from ip_calculator import IP_Calculator
+from  manual_config import Manual_config
+from mikrotik_connection import MikrotikConnection
 
-def main():
-    logging.basicConfig(filename='IP_Calculator.log', level=logging.INFO)
-    while True:
+
+def show_menu(index):
+    if index == 0: # MAIN MENU
         print("================================================================")
         print('= 1. IP Kalkulator                                             =')
         print("= 2. Konfigurasi                                               =")
-        print("= 3. Exit                                                      =")
+        print("= 0. Exit                                                      =")
+        print("================================================================")
+    elif index == 1:  #KONFIGURASI LAYER 1
+        print("================================================================")
+        print("=                      MENU KONFIGURASI                        =")
+        print("================================================================")
+        print("= 1. interface                                                 =")
+        print("= 2. IP                                                        =")
+        print("= 3. Queue                                                     =")
+        print("= 4. Firwall                                                   =")
+        print("= 5. hotspot                                                   =")
+        print("= 0. Back                                                      =")
+        print("================================================================")
+    elif index == 2: # KONFIGURASI -> INTERFACE
+        print("================================================================")
+        print("=                  KONFIGURASI INTERFACE                       =")
+        print("================================================================")
+        print("= 1. bridge                                                    =")
+        print("= 2. ethernet                                                  =")
+        print("= 3. wireless                                                  =")
+        print("= 0. Back                                                      =")
+        print("================================================================")
+    elif index == 3: # KONFIGURASI -> IP
+        print("================================================================")
+        print("=                     KONFIGURASI IP                           =")
+        print("================================================================")
+        print("= 1. address                                                   =")
+        print("= 2. dhcp-client                                               =")
+        print("= 3. wireless                                                  =")
+        print("= 0. Back                                                      =")
+        print("================================================================")
+    elif index == 4:# KONFIGURASI QUEUE
+        print("================================================================")
+        print("=                  KONFIGURASI QUEUE                           =")
+        print("================================================================")
+        print("= 1. DUMMY                                                     =")
+        print("= 2. DUMMY                                                     =")
+        print("= 0. Back                                                      =")
+        print("================================================================")
+    elif index == 5:# KONFIGURASI FIREWALL
+        print("================================================================")
+        print("=                  KONFIGURASI FIREWALL                        =")
+        print("================================================================")
+        print("= 1. DUMMY                                                     =")
+        print("= 2. DUMMY                                                     =")
+        print("= 0. Back                                                      =")
+        print("================================================================")
+    else:
+        print("================================================================")
+        print("=                  KONFIGURASI HOTSPOT                         =")
+        print("================================================================")
+        print("= 1. DUMMY hotspot                                             =")
+        print("= 2. DUMMY hotspot                                             =")
+        print("= 0. Back                                                      =")
         print("================================================================")
 
-        pilih = int(input("Masukkan Pilihan 1/2/3 : "))
-        if pilih == 1:
-            ip_address = input("Masukkan IP Address (Ex : 192.168.1.1) : ")
-            cidr = int(input("Masukkan Subnet(CIDR) range 8 - 32 : "))
 
-            if not 8 < cidr < 32:
-                print("Error : Angka CIDR yang diinputkan melebihi angka 32 atau kurang dari angka 8")
-                logging.error("Error: Angka yang diinput melebihi angka 32 atau kurang dari angka 8")
-            else:
-                calculator = IP_Calculator()
-                subnet_mask = calculator.cidr_to_subnet(cidr)
-                network = calculator.network_address(ip_address, subnet_mask)
-                broadcast = calculator.broadcast_address(network, subnet_mask)
-                usable_ip = calculator.usable_range(network, broadcast)
+def wait_for_input():
+    input("Tekan Enter untuk melanjutkan...")
 
-                ip_info = [
-                    ['IP Address            : ',ip_address],
-                    ['Subnet Mask           : ',subnet_mask],
-                    ['Network Address       : ',network],
-                    ['Broadcast             : ',broadcast],
-                    ['Usabel Rang           : ',usable_ip]
-                ]
 
-                for row in ip_info:
-                    print('{:<20} {}'.format(row[0], row[1]))
-                    logging.info(row[0] + ' ' + row[1])
-                input("Tekan enter")
-        elif pilih == 2:
-            hostname = input("IP Address Router Mikrotik : ")
-            username = input("Username : ")
-            password = input("Password : ")
-
-            connection = MikrotikConnection(hostname, username, password)
-            if connection.connect():
-                # print("Login berhasil!!!\n")
-                manual_config = Manual_config(connection)
-                print("================================================================")
-                print("=                      MENU KONFIGURASI                        =")
-                print("================================================================")
-                print("= 1. interface                                                 =")
-                print("= 2. PPP                                                       =")
-                print("= 3. IP                                                        =")
-                print("= 4. Queue                                                     =")
-                print("================================================================")
-                pilih = int(input("Masukkan Pilihan konfigurasi : "))
+def main():
+    index = 0
+    logging.basicConfig(filename='IP_Calculator.log', level=logging.INFO)
+    while True:
+        # os.system('clear')
+        show_menu(index)
+        pilih = input("Masukkan Pilihan : ")
+        if pilih.isdigit():
+            pilih = int(pilih)
+            if index == 0:
                 if pilih == 1:
-                    print("================================================================")
-                    print("=                  KONFIGURASI INTERFACE                       =")
-                    print("================================================================")
-                    print("= 1. bridge                                                    =")
-                    print("= 2. ethernet                                                  =")
-                    print("= 3. wireless                                                  =")
-                    print("================================================================")
+                    ip_address = input("Masukkan IP Address (Ex : 192.168.1.1) : ")
+                    cidr = int(input("Masukkan Subnet(CIDR) range 8 - 32 : "))
+                    if not 8 < cidr < 32:
+                        print("Error : Angka CIDR atau subnet yang diinputkan melebihi angka 32 atau dibawah angka 8")
+                        logging.error("Error: Angka yang diinputkan melebihi 32 atau dibawah angka 8")
+                    else:
+                        calculator = IP_Calculator()
+                        subnet_mask = calculator.cidr_to_subnet(cidr)
+                        network = calculator.network_address(ip_address, subnet_mask)
+                        broadcast = calculator.broadcast_address(network, subnet_mask)
+                        usable_ip = calculator.usable_range(network, broadcast)
+                        ip_info = [
+                            ['IP Address            : ',ip_address],
+                            ['Subnet Mask           : ',subnet_mask],
+                            ['Network Address       : ',network],
+                            ['Broadcast             : ',broadcast],
+                            ['Usable Rang           : ',usable_ip]
+                        ]
 
+                        for row in ip_info:
+                            print('{:<20} {}'.format(row[0], row[1]))
+                            logging.info(row[0] + ' ' + row[1])
+                    wait_for_input()
                 elif pilih == 2:
-                    print("================================================================")
-                    print("=                     KONFIGURASI PPP                          =")
-                    print("================================================================")
-                    print("= 1. bridge                                                    =")
-                    print("= 2. ethernet                                                  =")
-                    print("= 3. wireless                                                  =")
-                    print("================================================================")
-
+                    hostname = input("IP Address Router Mikrotik : ")
+                    username = input("Username : ")
+                    password = input("Password : ")
+                    connection = MikrotikConnection(hostname, username, password)
+                    if connection.connect():
+                        index += 1
+                    else:
+                        print("Login Gagal!!!")
+                elif pilih == 0:
+                    break
+            elif index == 1:
+                if 1 <= pilih <= 5:
+                    index += pilih
+                elif pilih == 0:
+                    index -= 1
+            elif index == 2:
+                if pilih == 1:
+                    print("KONFIGURASI -> INTERFACE -> BRIDGE")
+                    wait_for_input()
+                elif pilih == 2:
+                    print("KONFIGURASI -> INTERFACE -> ETHERNET")
+                    wait_for_input()
                 elif pilih == 3:
-                    print("================================================================")
-                    print("=                     KONFIGURASI IP                           =")
-                    print("================================================================")
-                    print("= 1. address                                                   =")
-                    print("= 2. dhcp-client                                               =")
-                    print("= 3. wireless                                                  =")
-                    print("================================================================")
-                    pilih = int(input("Pilih konfigurasi : "))
+                    print("KONFIGURASI -> INTERFACE -> WIRELESS")
+                    wait_for_input()
+            elif index == 3:
+                if pilih == 1:
+                    print("KONFIGURASI -> IP -> ADDRESS")
+                    wait_for_input()
+                elif pilih == 2:
+                    print("KONFIGURASI -> IP -> DHCP-CLIENT")
+                    wait_for_input()
+                elif pilih == 3:
+                    print("KONFIGURASI -> IP -> WIRELESS")
+                    wait_for_input()
+            elif index == 4:
+                if pilih == 1:
+                    print("KONFIGURASI -> QUEUE -> DUMMY1")
+                    wait_for_input()
+                elif pilih == 2:
+                    print("KONFIGURASI -> QUEUE -> DUMMY2")
+                    wait_for_input()
+            elif index == 5:
+                if pilih == 1:
+                    print("Dummy Firewall 1")
+                    wait_for_input()
+                elif pilih == 2:
+                    print("Dummy Firewall 2")
+                    wait_for_input()
+            elif index == 6:
+                if pilih == 1:
+                    print("Dummy Hotspot 1")
+                    wait_for_input()
+                elif pilih == 2:
+                    print("Dummy hotspot 2")
+                    wait_for_input()
+            if pilih == 0 and index > 1:
+                index = 1
 
-                    if pilih == 1:
-                        interface = input("Masukkan nama Interface : ")
-                        ip_address = input("Masukkan alamat IP : ")
-                        subnet_mask = input("Masukkan subnet mask : ")
-                        result = manual_config.Configure_IP(interface, ip_address, subnet_mask)
-                        # print(result)
-                        time.sleep(1)
-                        # connection.close_connection()
 
-            else:
-                print("Login Gagal")
-                break
-        elif pilih == 3:
-            break
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- manual_config.py
+++ manual_config.py
@@ -2,7 +2,7 @@ class Manual_config:
     def __init__(self, mikrotik_connection):
         self.connection = mikrotik_connection
 
-    def Change_Interface_Name(self, interface, new_name):
+    def change_interface_name(self, interface, new_name):
         command = f'/interface set {interface} name={new_name}'
         try:
             result = self.connection.execute_command(command)
@@ -11,7 +11,7 @@ class Manual_config:
         else:
             print(f"Interface {interface} berhasil dirubah menjadi {new_name}")
 
-    def Create_Bridge(self,name_bridge):
+    def create_bridge(self,name_bridge):
         command = f'/interface bridge add fast-forward=no name={name_bridge}'
         try:
             result = self.connection.execute_command(command)
@@ -20,7 +20,7 @@ class Manual_config:
         else:
             print(f"Interface bridge berhasil dibuat dengan nama {name_bridge}")
 
-    def Add_Bridge_Port(self, name_bridge, interface):
+    def add_bridge_port(self, name_bridge, interface):
         jumlah = int(input("Masukkan berapa port yang akan dimasukkan dalam interface bridge :"))
         for i in range(jumlah):
             command = f'/interface bridge port add bridge={name_bridge} interface={interface}'
@@ -31,8 +31,7 @@ class Manual_config:
             else:
                 print(f"Interface {interface} berhasil ditambahkan di bridge {name_bridge}")
 
-
-    def Configure_IP(self, interface, ip_address, subnet_mask):
+    def configure_ip(self, interface, ip_address, subnet_mask):
         command = f'/ip address add interface={interface} address={ip_address}/{subnet_mask}'
         try:
             result = self.connection.execute_command(command)
@@ -41,7 +40,16 @@ class Manual_config:
         else:
             print(f"IP Address {ip_address}/{subnet_mask} berhasil ditambahkan diinteface {interface}")
 
-    def DHCP_Client(self,interface):
+    def configure_dns(self, server):
+        command = f'/ip dns set servers={server} allow-remote-requests=yes'
+        try:
+            result = self.connection.execute_command(command)
+        except Exception as e:
+            print("Terjadi Kesalahan",str(e))
+        else:
+            print(f"DNS berhasil ditambahkan dengan DNS Server {server}")
+
+    def dhcp_client(self,interface):
         command = f'/ip dhcp-client add disable=no interface={interface}'
         try:
             result = self.connection.execute_command(command)
@@ -50,14 +58,14 @@ class Manual_config:
         else:
             print(f"DHCP Client berhasil diterapkan di interface {interface}")
 
-    def IP_POOL(self, pool_name, ranges):
+    def configure_ip_pool(self, pool_name, ranges):
         command = f'/ip pool add name={pool_name} ranges={ranges}'
         try:
             result = self.connection.execute_command(command)
         except Exception as e:
             print(f"IP Pool berhasil ditambahkan dengan nama {pool_name} dengan ranges {ranges}")
 
-    def DHCP_Server(self, pool_name, interface, name_dhcp):
+    def dhcp_server(self, pool_name, interface, name_dhcp):
         command = f'/ip dhcp-server add address-pool={pool_name} disabled=no interface={interface} lease-time=1h name={name_dhcp}'
         try:
             result = self.connection.execute_command(command)
@@ -65,3 +73,30 @@ class Manual_config:
             print("Terjadi Kesalahan",str(e))
         else:
             print(f"DHCP Server berhasil ditambahkan dengan nama {name_dhcp} dan menggunakan pool {pool_name}")
+
+    def configure_dhcp_network(self,network_address, gateway):
+        command = f'/ip dhcp-server network add address={network_address} gateway={gateway}'
+        try:
+            result = self.connection.execute_command(command)
+        except Exception as e:
+            print("Terkjadi Kesalahan",str(e))
+        else:
+            print(f'DHCP Network berhasil ditambahkan dengan network address {network_address}\nserta gateway address {gateway}')
+
+    def masquerade_nat(self, src_address):
+        command = f'/ip firewall nat add action=masquerade chain=srcnat src-address={src_address}'
+        try:
+            result = self.connection.execute_command(command)
+        except Exception as e:
+            print("Terjadi Kesalahan",str(e))
+        else:
+            print(f"Firewall Nat Maquerade dapat ditambahkan dengan src-address:{src_address}")
+
+    def configure_firewall_raw(self, action, address_list, address_list_timeout,chain, comment, content, dst_address_list, src_address_list):
+        command = f'/ip firewall raw add action={action} address-list={address_list} address-list-timeout={address_list_timeout} chain={chain} comment={comment} content={content} dst-address-list={dst_address_list} src-address-list={src_address_list}'
+        try:
+            result = self.connection.execute_command(command)
+        except Exception as e:
+            print("Terjadi Kesalahan",str(e))
+        else:
+            print(f'Firewall RAW berhasil ditambahkan untuk Konten {content}')
